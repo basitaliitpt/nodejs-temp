bda8718af6502e2546f64f620dd8475e62213fe6
Basit Ali <basit.ali@itpt.local>
2022-01-15 04:44:17 +0000

    app.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+app.get("/", function(req, res){
+  res.send("<h1> hEllo w </h1>");
+});
+app.get("/contact", function(req, res){
+  res.send("Contact me at basit@itpt.co.uk");
+});
+app.listen(3000, function(){
+  console.log("Server has started at port 3000");
+})
